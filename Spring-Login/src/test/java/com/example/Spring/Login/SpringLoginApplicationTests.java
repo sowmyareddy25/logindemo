e379d0a65a0b751f4c621553d8b201eeead00cac
kudlamolka <moleyarcentral@gmail.com>
2021-09-22 06:32:52 +0530

    login with token

---
+++ Spring-Login/src/test/java/com/example/Spring/Login/SpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- logindemo/src/main/webapp/WEB-INF/home.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<head></head>
-<body>
-<h1> WELCOME TO HOME ${UserName}</h1>
-</body>
-</html>
